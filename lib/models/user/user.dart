d031a18d6d5549ae3d200d916cddb906cfe355ef
Joseph Cheng <the_only_light@hotmail.com>
2021-06-16 20:37:04 +0800

    Implement User model

---
+++ lib/models/user/user.dart
@@ -0,0 +1,19 @@
+class User {
+  final String email;
+  final String uid;
+  final String displayName;
+
+  User({
+    required this.uid,
+    required this.email,
+    required this.displayName,
+  });
+
+  User copyWith({String? uid, String? email, String? displayName}) {
+    return User(
+      uid: uid ?? this.uid,
+      email: email ?? this.email,
+      displayName: displayName ?? this.displayName,
+    );
+  }
+}
